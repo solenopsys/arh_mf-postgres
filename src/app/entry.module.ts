e21eacf762e56287ab06b9b171b994008e4df256
AlexStorm <info@alexstorm.ru>
2023-06-19 12:48:21 +0530

    refactor modules

--- src/app/entry.module.ts
+++ src/app/entry.module.ts
@@ -1,10 +1,31 @@
-import { NgModule } from '@angular/core';
 
-import { RemoteEntryComponent } from './entry.component';
-import {IMPORTS_CONF, PROVIDERS_CONF} from "./app.module";
 
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
+import {TABLE_PAGE} from "@solenopsys/ui-templates";
+import {FormsModule} from "@angular/forms";
+import {TABLES} from "./tables.config";
+import {CommonModule} from "@angular/common";
+
+const routes: Routes = [
+  TABLE_PAGE(':table'),
+];
+
+export const PROVIDERS_CONF = [
+  {provide: 'tables', useValue: TABLES},
+  {provide: 'assets_dir', useValue: "/fm/modules/mf-postgres"},
+  {provide: 'mod_name', useValue: "git"}
+]
+
+export const IMPORTS_CONF = [
+  BrowserModule,
+  CommonModule,
+  RouterModule.forChild(routes),
+  FormsModule,
+]
 @NgModule({
-  declarations: [RemoteEntryComponent],
+  declarations: [],
   imports: [
     ...IMPORTS_CONF
   ],
